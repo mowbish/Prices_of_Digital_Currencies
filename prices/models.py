53737c4e319512e79ed88e32d6d872aae806d0f1
Mobin <mobin.clash.ma@gmail.com>
2022-02-23 15:53:34 +0330

    Added models of prices app

--- prices/models.py
+++ prices/models.py
@@ -1,3 +1,15 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
-# Create your models here.
+
+class User(AbstractUser):
+    pass
+
+
+class FileDetail(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    file = models.FileField()
+    upload_date = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.user.username
