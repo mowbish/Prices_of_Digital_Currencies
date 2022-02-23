73b29a42b7378aa7e807597565dc9e7a47a000a3
Mobin <mobin.clash.ma@gmail.com>
2022-02-23 17:31:04 +0330

    Changes on upload path

--- prices/models.py
+++ prices/models.py
@@ -8,7 +8,7 @@ class User(AbstractUser):
 
 class FileDetail(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
-    file = models.FileField()
+    file = models.FileField(upload_to='uploads/')
     upload_date = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
