ea24ab5ec1b5361be05b6c869cecd306004a7e75
Mobin <mobin.clash.ma@gmail.com>
2022-02-23 17:31:40 +0330

    Nothing to commit

--- prices/urls.py
+++ prices/urls.py
@@ -1,5 +1,8 @@
+from django.urls import path
 
+from prices.views import FileUploadView, UserSignUpAPIView
 
 urlpatterns = [
-
+    path('', FileUploadView.as_view(), name='upload-file'),
+    path('signup/', UserSignUpAPIView.as_view(), name='signup')
 ]
