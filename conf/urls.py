617722c2b25cb93f6e2586786c0e35e556d1ea4e
Mobin <mobin.clash.ma@gmail.com>
2022-02-23 16:00:19 +0330

    Added DRF(django rest framework) to project

--- conf/urls.py
+++ conf/urls.py
@@ -19,4 +19,5 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('/', include('prices.urls')),
+    path('api-auth/', include('rest_framework.urls'))
 ]
