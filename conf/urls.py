b51f1e1717ba812f560970c2b86dcc9c281b8b7d
Mobin <mobin.clash.ma@gmail.com>
2022-02-23 17:32:21 +0330

    Nothing to commit

--- conf/urls.py
+++ conf/urls.py
@@ -18,6 +18,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('/', include('prices.urls')),
+    path('', include('prices.urls')),
     path('api-auth/', include('rest_framework.urls'))
 ]
