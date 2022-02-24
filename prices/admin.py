ad1402351d3e316292848ff4fdf19fe246efd1dc
Mobin <77534498+mowbish@users.noreply.github.com>
2022-02-24 22:13:12 +0330

    Added UserAdmin and FileDetailAdmin
    UserAdmin and FileDetailAdmin classes are added to use better admin panel by superuser

--- prices/admin.py
+++ prices/admin.py
@@ -1,3 +1,23 @@
 from django.contrib import admin
 
-# Register your models here.
+from django.contrib import admin
+from .models import User, FileDetail
+
+
+@admin.register(User)
+class UserAdmin(admin.ModelAdmin):
+    list_display = ("username", "first_name", "last_name", "email", "date_joined")
+    search_fields = ('username', "first_name", "last_name", "email",)
+    empty_value_display = '-empty-'
+    ordering = ('date_joined',)
+
+
+@admin.register(FileDetail)
+class FileDetailAdmin(admin.ModelAdmin):
+    list_display = ("user", "file", "upload_date")
+    list_filter = ('upload_date',)
+    date_hierarchy = 'upload_date'
+    search_fields = ("file",)
+    empty_value_display = '-empty-'
+    ordering = ('upload_date',)
+
